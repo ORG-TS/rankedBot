d72105f49a9ce695132480c84c3448ed7974e5f8
d3mios <d3imosxd@gmail.com>
2025-03-28 21:02:39 +0100

    Racha y victorias/partidas totales
    Añadidos campos para la racha del jugador y su total de victorias y partidas jugadas por temporada.

--- BD/models/Jugador.js
+++ BD/models/Jugador.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 
 const PuntosPorTemporadaSchema = new mongoose.Schema({
     temporada: { type: Number, required: true, unique: true },
-    puntos: { type: Number, default: 0 }
+    puntos: { type: Number, default: 0 },
+    victoriasTotales: { type: Number, default: 0 },
+    partidasJugadas: { type: Number, default: 0}
 })
 
 const JugadorSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
     puntuaciones: [PuntosPorTemporadaSchema],
     ultimaWin: { ref: Jugador },
+    racha: { type: Number, default: 0 },
     mains: [ { type: String } ]
 });
 
